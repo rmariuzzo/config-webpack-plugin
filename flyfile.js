94630b63f1f2ec0d7232443189c859ca318c59d9
Rubens Mariuzzo <rubens@mariuzzo.com>
2016-08-09 00:22:54 -0400

    Fly implementation.

--- flyfile.js
+++ flyfile.js
@@ -1,5 +1,40 @@
-module.exports.default = function * () {
+var path = require('path');
+
+var task = module.exports,
+    target = {
+        plugin: 'src/config-webpack-plugin.js',
+        loader: 'src/config-loader.js',
+        lib: 'lib/'
+    };
+
+/**
+ * The build task.
+ */
+task.build = function* () {
+    /** @desc The build task used to generate the library. */
+
     yield this
-        .source('src/*.js')
-        .target('lib');
+        .clear(target.lib);
+
+    yield this
+        .source([target.plugin, target.loader])
+        .eslint();
+
+    var babelized = this
+        .source(target.plugin)
+        .babel();
+
+    yield babelized
+        .target(target.lib);
+
+    yield babelized
+        .uglify()
+        .target(prefix(target.plugin, '.min'));
+};
+
+/* Functions. */
+
+function prefix(file, prefix) {
+    file = path.basename(file);
+    return file.replace(/\.\w+$/, ext => `${prefix}${ext}`);
 }
--- src/config-loader.js
+++ src/config-loader.js
@@ -1,4 +1,4 @@
-module.exports = () => {
+module.exports = function () {
     let config = JSON.parse(this.query.slice(1));
     return `module.exports = ${JSON.stringify(config)}`;
 };

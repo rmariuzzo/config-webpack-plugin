190e690ec35a75c48cad21e6cdd8a0de68ca200f
Rubens Mariuzzo <rubens@mariuzzo.com>
2016-08-09 02:08:31 -0400

    v1.0.1.

--- src/config-loader.js
+++ src/config-loader.js
@@ -1,3 +1,8 @@
+/**
+ * The ConfigLoader function.
+ *
+ * @author Rubens Mariuzzo <rubens@mariuzzo.com>
+ */
 module.exports = function () {
     let config = JSON.parse(this.query.slice(1));
     return `module.exports = ${JSON.stringify(config)}`;
